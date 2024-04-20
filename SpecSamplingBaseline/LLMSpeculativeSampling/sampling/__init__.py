3000242be9758b56391cf672b108fc42d2700ce6
JordannnnnPeng <72975739+JordannnnnPeng@users.noreply.github.com>
2024-04-19 23:34:41 -0400

    0419_baseline

---
+++ SpecSamplingBaseline/LLMSpeculativeSampling/sampling/__init__.py
@@ -0,0 +1,4 @@
+from sampling.speculative_sampling import speculative_sampling, speculative_sampling_v2,speculative_sampling_spec_tree
+from sampling.autoregressive_sampling import autoregressive_sampling
+
+__all__ = ["speculative_sampling", "speculative_sampling_v2", "autoregressive_sampling","speculative_sampling_spec_tree"]
