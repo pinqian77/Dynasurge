be69863928e938290ce801db08b7823d943f297f
pinqian77 <pinqian77@gmail.com>
2024-04-25 06:40:04 +0000

    add verbose mode for decoding

--- Dynasurge/eval.sh
+++ Dynasurge/eval.sh
@@ -3,32 +3,54 @@
 mkdir -p log
 log_file="log/$(date +%Y%m%d%H%M)_results.log"
 
+echo "###### Test Eval Pipeline #######"
 echo "########## Dataset cnn ##########" >> $log_file
 echo "========= Autoregressive ========" >> $log_file
-CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 2 --M 384 --mode auto --dataset cnn >> $log_file
+CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 4 --M 384 --mode auto --dataset cnn --verbose >> $log_file
 
 echo "========== Static Tree ==========" >> $log_file
-CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 2 --M 384 --growmap ./growmaps/4x8-tree.pt  --mode sTree --dataset cnn >> $log_file
-CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 2 --M 384 --use_bfs --growmap ./growmaps/8x8-tree.pt  --mode sTree --dataset cnn >> $log_file
+echo "-------------- 4x8-tree --------------" >> $log_file
+CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 4 --M 384 --growmap ./growmaps/4x8-tree.pt  --mode sTree --dataset cnn --verbose >> $log_file
+CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 4 --M 384 --use_bfs --growmap ./growmaps/4x8-tree.pt  --mode sTree --dataset cnn --verbose >> $log_file
 
-CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 2 --M 384 --growmap ./growmaps/8x8-tree.pt  --mode sTree --dataset cnn >> $log_file
-CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 2 --M 384 --use_bfs --growmap ./growmaps/8x8-tree.pt  --mode sTree --dataset cnn >> $log_file
-
-CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 2 --M 384 --growmap ./growmaps/demo_tree.pt  --mode sTree --dataset cnn >> $log_file
-CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 2 --M 384 --use_bfs --growmap ./growmaps/demo_tree.pt  --mode sTree --dataset cnn >> r$log_file
-
+echo "-------------- 8x8-tree --------------" >> $log_file
+CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 4 --M 384 --growmap ./growmaps/8x8-tree.pt  --mode sTree --dataset cnn --verbose >> $log_file
+CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 4 --M 384 --use_bfs --growmap ./growmaps/8x8-tree.pt  --mode sTree --dataset cnn --verbose >> $log_file
 
 
-echo "########## Dataset wiki ##########" >> $log_file
-echo "========= Autoregressive ========" >> $log_file
-CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 2 --M 384 --mode auto --dataset wiki >> $log_file
+# echo "########## Dataset cnn ##########" >> $log_file
+# echo "========= Autoregressive ========" >> $log_file
+# CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --mode auto --dataset cnn >> $log_file
 
-echo "========== Static Tree ==========" >> $log_file
-CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 2 --M 384 --growmap ./growmaps/4x8-tree.pt  --mode sTree --dataset wiki >> $log_file
-CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 2 --M 384 --use_bfs --growmap ./growmaps/8x8-tree.pt  --mode sTree --dataset wiki >> r$log_file
+# echo "========== Static Tree ==========" >> $log_file
+# echo "-------------- 4x8-tree --------------" >> $log_file
+# CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ./growmaps/4x8-tree.pt  --mode sTree --dataset cnn >> $log_file
+# CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --use_bfs --growmap ./growmaps/4x8-tree.pt  --mode sTree --dataset cnn >> $log_file
 
-CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 2 --M 384 --growmap ./growmaps/8x8-tree.pt  --mode sTree --dataset wiki >> $log_file
-CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 2 --M 384 --use_bfs --growmap ./growmaps/8x8-tree.pt  --mode sTree --dataset wiki >> r$log_file
+# echo "-------------- 8x8-tree --------------" >> $log_file
+# CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ./growmaps/8x8-tree.pt  --mode sTree --dataset cnn >> $log_file
+# CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --use_bfs --growmap ./growmaps/8x8-tree.pt  --mode sTree --dataset cnn >> $log_file
 
-CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 2 --M 384 --growmap ./growmaps/demo_tree.pt  --mode sTree --dataset wiki >> $log_file
-CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 2 --M 384 --use_bfs --growmap ./growmaps/demo_tree.pt  --mode sTree --dataset wiki >> $log_file
+# echo "-------------- demo-tree --------------" >> $log_file
+# CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ./growmaps/demo_tree.pt  --mode sTree --dataset cnn >> $log_file
+# CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --use_bfs --growmap ./growmaps/demo_tree.pt  --mode sTree --dataset cnn >> r$log_file
+# echo
+
+
+# echo "########## Dataset wiki ##########" >> $log_file
+# echo "========= Autoregressive ========" >> $log_file
+# CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --mode auto --dataset wiki >> $log_file
+
+# echo "========== Static Tree ==========" >> $log_file
+# echo "-------------- 4x8-tree --------------" >> $log_file
+# CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ./growmaps/4x8-tree.pt  --mode sTree --dataset wiki >> $log_file
+# CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --use_bfs --growmap ./growmaps/4x8-tree.pt  --mode sTree --dataset wiki >> r$log_file
+
+# echo "-------------- 8x8-tree --------------" >> $log_file
+# CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ./growmaps/8x8-tree.pt  --mode sTree --dataset wiki >> $log_file
+# CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --use_bfs --growmap ./growmaps/8x8-tree.pt  --mode sTree --dataset wiki >> r$log_file
+
+# echo "-------------- demo-tree --------------" >> $log_file
+# CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ./growmaps/demo_tree.pt  --mode sTree --dataset wiki >> $log_file
+# CUDA_VISIBLE_DEVICES=0 python main.py --draft  JackFram/llama-68m   --target huggyllama/llama-7b  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --use_bfs --growmap ./growmaps/demo_tree.pt  --mode sTree --dataset wiki >> $log_file
+# echo
--- Dynasurge/utils/decoding.py
+++ Dynasurge/utils/decoding.py
@@ -15,7 +15,7 @@ def Autoregressive(target_model : InferenceEngine, dataloader: DataLoader, token
     num_decoding_steps = 0
     total_time = 0.0
 
-    for _, batch in tqdm(enumerate(dataloader), total=num_eval_steps):        
+    for data_id, batch in tqdm(enumerate(dataloader), total=num_eval_steps):        
         input_ids = batch['input_ids'][..., :128]
         terminate = True if input_ids[0][-1] == 2 else False            
         position_ids = torch.arange(max_length).to('cuda:0').unsqueeze(0)
@@ -24,27 +24,11 @@ def Autoregressive(target_model : InferenceEngine, dataloader: DataLoader, token
         torch.cuda.synchronize()
         t1 = time.time()
         inner_decoding_step = 0
-        start_length = 0
+        start_length = input_ids.shape[1]
 
-        # if verbose:
-        #     input_text = (
-        #             tokenizer.decode(
-        #             input_ids[0], 
-        #             skip_special_tokens=True,
-        #             clean_up_tokenization_spaces=True,
-        #             space_between_special_tokens=False
-        #             )
-        #             .strip()
-        #             .split(" ")
-        #         )
-        #     print(" ".join(input_text), end=" ", flush=True)
-        
-        # pos = 0
-        # generated_ids = []
-        while inner_decoding_step < 128 and not terminate:
-            # prefill for first token
+        while inner_decoding_step + 128 < (max_length - start_length) and not terminate: # TODO: should fix Sequioa bug. current max length is max_length - 128
+        # while inner_decoding_step < (max_length - start_length) and not terminate:
             if inner_decoding_step == 0:
-                start_length = input_ids.shape[1]
                 logits = target_model.inference(input_ids = input_ids, storage_ids=storage_ids[:start_length],
                                                 position_ids = position_ids[..., :start_length], 
                                                 attn_mask=attn_mask[:start_length, :start_length][None, None, :, :])[0][-1]
@@ -53,7 +37,7 @@ def Autoregressive(target_model : InferenceEngine, dataloader: DataLoader, token
                                                 position_ids = position_ids[..., start_length + inner_decoding_step-1 : start_length + inner_decoding_step], 
                                                 attn_mask=attn_mask[start_length + inner_decoding_step-1 : start_length + inner_decoding_step, :start_length + inner_decoding_step][None, None, :, :])[0][-1]
             
-            # Adjust logits based on temperature and top-p filtering, then sample a new token
+            # Adjust logits and sample a new token
             logits = get_sampling_logits(logits=logits, top_p=top_p, T=T)
             p = softmax(logits / T, dim=-1)
             new_token = p.multinomial(num_samples=1).unsqueeze(0)
@@ -62,31 +46,19 @@ def Autoregressive(target_model : InferenceEngine, dataloader: DataLoader, token
             num_decoding_steps += 1
             inner_decoding_step += 1
 
-            # Check if the new token is the end-of-sequence token
             if input_ids[0][-1] == 2: 
                 terminate = True
-            
-            # if verbose:
-            #     generated_ids.extend(input_ids[0][-1:].tolist())
-            #     generated_text = (
-            #         tokenizer.decode(
-            #         generated_ids,
-            #         skip_special_tokens=True,
-            #         clean_up_tokenization_spaces=True,
-            #         space_between_special_tokens=False,
-            #         ).strip().split(" ")
-            #     )
-            #     now = len(generated_text) - 1
-            #     if now > pos:
-            #         print(" ".join(generated_text[pos:]), end=" ", flush=True)
-            #         pos = now
 
         torch.cuda.synchronize()
         t2 = time.time()
         total_time += (t2 - t1)
         target_model.clear_kv()
+        
+        if verbose and num_decoding_steps > 0:
+            print(f"data id: {data_id} | time cost: {t2 - t1:.5f}s | final length: {start_length + inner_decoding_step} | decoding step: {inner_decoding_step} | latency per token: {(t2 - t1) / inner_decoding_step:.5f}s")
             
-    print("total time :{:.5f}s, latency :{:.5f}s, decoding step: {}, latency per request: {:5f}".format(total_time, total_time / num_decoding_steps, num_decoding_steps, total_time / num_eval_steps))
+    # print("total time :{:.5f}s, latency :{:.5f}s, decoding step: {}, latency per request: {:5f}".format(total_time, total_time / num_decoding_steps, num_decoding_steps, total_time / num_eval_steps))
+    print(f"total time: {total_time:.5f}s | total decoding step: {num_decoding_steps} | latency per token: {total_time / num_decoding_steps:.5f}s | latency per request: {total_time / num_eval_steps:.5f}")
     return num_decoding_steps
 
 
@@ -105,7 +77,7 @@ def Sequoia(target_model : InferenceEngine, draft_model: InferenceEngine, datalo
     parents_buffer =  torch.zeros(max_length).long().to('cuda:0')
     position_ids = torch.zeros(max_length).long().to('cuda:0')
     
-    for _, batch in tqdm(enumerate(dataloader), total=num_eval_steps):
+    for data_id, batch in tqdm(enumerate(dataloader), total=num_eval_steps):
         input_ids = batch['input_ids'][..., :128]
         terminate = True if input_ids[0][-1] == 2 else False
         draft_kv_len = 0
@@ -124,10 +96,12 @@ def Sequoia(target_model : InferenceEngine, draft_model: InferenceEngine, datalo
         torch.cuda.synchronize()
         t1 = time.time()
 
-        if verbose:
-            pass
-
-        while input_ids.shape[1] < 256 and not terminate:
+        curr_decoding_steps = 0
+        curr_large_model_steps = 0
+        start_length = input_ids.shape[1]
+        # while input_ids.shape[1] < 256 and not terminate:
+        while input_ids.shape[1] < (max_length - start_length) and not terminate:
+            # print(f"input_ids shape: {input_ids.shape[1]}, max_length: {max_length}, start_length: {start_length}, max_length - start_length: {max_length - start_length}")
             spectree.construct_grow_map()
             
             if not bfs_verify:
@@ -135,22 +109,26 @@ def Sequoia(target_model : InferenceEngine, draft_model: InferenceEngine, datalo
             else:
                 valid_tokens, draft_kv_len, target_kv_len, terminate = spectree.verify_bfs()
             
-            num_decoding_steps += (valid_tokens.shape[0] - input_ids.shape[1])
-            num_large_model_steps += 1
+            curr_decoding_steps += (valid_tokens.shape[0] - input_ids.shape[1])
+            curr_large_model_steps += 1
             input_ids = valid_tokens.unsqueeze(0)
             if (input_ids[0][-1] == 2) or (input_ids[0][-1] == 0): 
                 terminate = True
-
-            if verbose:
-                pass
         
+        num_decoding_steps += curr_decoding_steps
+        num_large_model_steps += curr_large_model_steps
+
         torch.cuda.synchronize()
         t2 = time.time()
         total_time += (t2 - t1)
         draft_model.clear_kv()
         target_model.clear_kv()
+
+        if verbose and num_decoding_steps > 0:
+            print(f"data id: {data_id} | time cost: {t2 - t1:.5f}s | final length: {start_length + curr_decoding_steps} | total decoding step: {curr_decoding_steps} | large model step: {curr_large_model_steps} | latency per token: {(t2 - t1) / curr_decoding_steps:.5f}s")
         
-    print("total time :{:.5f}s, latency :{:.5f}s, decoding step: {}, large model step: {}, accept rate: {:.5f}, latency per request: {:5f}".format(total_time, total_time / num_decoding_steps, num_decoding_steps, num_large_model_steps, num_decoding_steps / num_large_model_steps, total_time / num_eval_steps))
+    # print("total time :{:.5f}s, latency :{:.5f}s, decoding step: {}, large model step: {}, accept rate: {:.5f}, latency per request: {:5f}".format(total_time, total_time / num_decoding_steps, num_decoding_steps, num_large_model_steps, num_decoding_steps / num_large_model_steps, total_time / num_eval_steps))
+    print(f"total time: {total_time:.5f}s | total decoding step: {num_decoding_steps} | large model step: {num_large_model_steps} | latency per token: {total_time / num_decoding_steps:.5f}s | latency per request: {total_time / num_eval_steps:.5f} | accept rate: {num_decoding_steps / num_large_model_steps:.5f}")
     return num_decoding_steps / num_large_model_steps
 
 
